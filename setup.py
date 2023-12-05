8ae725c941bf3cd2ddf0c7de87aead42b3737c3b
Noel Soares <noelsoares408@gmail.com>
2023-12-06 02:20:30 +0530

    2nd commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this porject will recommend the top 3 dishes on the swiggy homepage',
+    author='Noel Soares',
+    license='',
+)
